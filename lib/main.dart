cfd4d2ce8397d66bbaef6e486e34860359b0021f
zoe park <hellopark821@gmail.com>
2022-12-14 12:36:08 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StorePage(),
+    );
+  }
+}
+
+class StorePage extends StatelessWidget {
+  const StorePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Row(
+                children: [
+                  Text("Woman", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Kids", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Shoes", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Spacer(),
+                  Text("Bag", style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+            Image.asset("assets/pairi.png", fit: BoxFit.cover),
+            Image.asset("assets/pairi.png", fit: BoxFit.cover),
+          ],
+        ),
+      ),
+    );
+  }
+}
